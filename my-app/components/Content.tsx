88f771bccd1672e685a4c941495851136c4caeb6
asfund <asfundmirzaj@gmail.com>
2023-09-01 06:59:50 +0500

    added data structures and functions

--- my-app/components/Content.tsx
+++ my-app/components/Content.tsx
@@ -4,9 +4,13 @@ import React, { useEffect } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function Content() {
-  const getBoard = useBoardStore((state) => state.getBoard);
+  const [getBoard, board] = useBoardStore((state) => [
+    state.getBoard,
+    state.board,
+  ]);
   useEffect(() => {
     getBoard();
+    console.log(board);
   }, [getBoard]);
   return (
     <div>Hello</div>
